a01786b2406515a43ff74e15efe08b8e1f5aae9a
muhammad nameer farooq <nameerfarooquit@gmail.com>
2021-05-27 12:24:23 -0700

    ok

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (Scaffold(
+          body: Text("MUHAMMAD NAMEER FAROOQ, nameerfarooq18@gmail.com"))),
+    );
+  }
+}
